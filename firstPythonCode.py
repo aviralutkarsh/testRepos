127545a22e68b4b7f506a2acf15f5351616d7c55
aviralutkarsh <69536683+aviralutkarsh@users.noreply.github.com>
2020-08-12 00:48:04 +0530

    Add files via upload
    My introduction to everyone!

---
+++ firstPythonCode.py
@@ -0,0 +1,8 @@
+# A simple function to introduce me. My first try at Python!
+
+def greeting(name, job):
+  print("Hello everyone! I am " + name + ".")
+  print("I work in " + job + " department.")
+  print("I am pleased to meet you all!")
+
+greeting("Aviral Utkarsh", "Data Analytics")
