44b090b54621a07fc71027b1403c63babc04d5fb
aviralutkarsh <69536683+aviralutkarsh@users.noreply.github.com>
2020-08-12 01:25:36 +0530

    Create secondPythonCode.py
    Introducing my friend.

---
+++ secondPythonCode.py
@@ -0,0 +1,8 @@
+# A simple function to introduce my friend and colleague. My second try at Python coding!
+
+def greeting(name, job):
+  print("Hello everyone! I am " + name + ".")
+  print("I work in " + job + " department.")
+  print("I am pleased to meet you all!")
+
+greeting("Bhavesh Pawar", "SAP Consultant")
